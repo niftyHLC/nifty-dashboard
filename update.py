f4af87147def40b38ec9d9b96532e21ae9737ff9
niftyHLC <117426821+niftyHLC@users.noreply.github.com>
2026-09-13 20:22:51 +0530

    Update update.py

--- update.py
+++ update.py
@@ -33,9 +33,9 @@ def get_market_holidays():
             datetime.date(2026, 6, 26),   # Muharram
             datetime.date(2026, 9, 14),   # Ganesh Chaturthi
             datetime.date(2026, 10, 2),   # Mahatma Gandhi Jayanti
-            datetime.date(2026, 10, 20), # Dussehra
-            datetime.date(2026, 11, 10), # Diwali-Balipratipada
-            datetime.date(2026, 11, 24), # Prakash Gurpurb
+            datetime.date(2026, 10, 20),  # Dussehra
+            datetime.date(2026, 11, 10),  # Diwali-Balipratipada
+            datetime.date(2026, 11, 24),  # Prakash Gurpurb
             datetime.date(2026, 12, 25)   # Christmas
         })
         
