e6105979a39f9d73938b4681d2a901aa83178daa
niftyHLC <117426821+niftyHLC@users.noreply.github.com>
2026-09-23 19:17:38 +0530

    Update update.py

--- update.py
+++ update.py
@@ -431,15 +431,9 @@ def calculate_dominance_metrics(data_dict):
             "tagClass": "tag-neutral"
         }
     
-    close = data_dict.get("close", 0.0)
     high = data_dict.get("high", 0.0)
-    if high <= 0:
-        high = close
-        
     low = data_dict.get("low", 0.0)
-    if low <= 0:
-        low = close
-        
+    close = data_dict.get("close", 0.0)
     iv = data_dict.get("iv", 0.0)
     
     hc = round(high - close, 2)
